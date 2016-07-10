29f460bfb3e72d7ca6b6d0f44881a73958f826da
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2016-07-10 17:01:42 -0300

    hand with numbers and suits separated and counted

--- src/hand.py
+++ src/hand.py
@@ -1,4 +1,6 @@
 # encoding: utf-8
+import re
+import collections
 
 
 class Hand(object):
@@ -6,9 +8,22 @@ class Hand(object):
     cards = []
 
     def __init__(self, cards):
+        self.numbers = collections.defaultdict(int)
+        self.suits = collections.defaultdict(int)
         self.cards = cards
+        self._set_numbers_and_suits()
 
     @staticmethod
     def from_string(cards_str):
         cards = cards_str.split(" ")
         return Hand(cards)
+
+    def _set_numbers_and_suits(self):
+        for card in self.cards:
+            match = re.match(r"([24556789TJQKA]+)([CDHS]+)", card, re.I)
+            if not match:
+                raise Exception('wrong card defined')
+            else:
+                values = match.groups()
+                self.numbers[values[0]] += 1
+                self.suits[values[1]] += 1
--- src/test.py
+++ src/test.py
@@ -4,9 +4,26 @@ from hand import Hand
 
 class TestPokerHand(unittest.TestCase):
 
-    def test_hand_should_be_created_by_method_from_string(self):
+    def test_hand_must_be_created_by_method_from_string(self):
         hand = Hand.from_string('4D 4D 4D 7H 8D')
         self.assertTrue(isinstance(hand, Hand))
+
+    def test_hand_wrong_card_parameter_should_raise_exception(self):
+        with self.assertRaises(Exception) as cm:
+            Hand.from_string('4D 4D 4D 99k 8D')
+        self.assertEquals(cm.exception.message, 'wrong card defined')
+
+    def test_hand_must_count_numbers(self):
+        hand = Hand.from_string('4D 4D 4D 7H 8D')
+        self.assertEquals(hand.numbers['4'], 3)
+        self.assertEquals(hand.numbers['7'], 1)
+        self.assertEquals(hand.numbers['8'], 1)
+
+    def test_hand_must_count_suits(self):
+        hand = Hand.from_string('4D 4D 4D 7H 8D')
+        self.assertEquals(hand.suits['D'], 4)
+        self.assertEquals(hand.suits['H'], 1)
+
 
 if __name__ == '__main__':
     unittest.main()
