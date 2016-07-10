a5bed76bc5cc476330531c02910a04935a40e0bd
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2016-07-10 20:38:47 -0300

    get a value of a hand

--- src/hand.py
+++ src/hand.py
@@ -4,7 +4,7 @@ import collections
 
 
 class Hand(object):
-
+    possible_numbers = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
     cards = []
 
     def __init__(self, cards):
@@ -16,17 +16,54 @@ class Hand(object):
         self.cards = cards
         self._set_numbers_and_suits()
 
-    @staticmethod
-    def from_string(cards_str):
-        cards = cards_str.split(" ")
-        return Hand(cards)
-
     def _set_numbers_and_suits(self):
         for card in self.cards:
-            match = re.match(r"([24556789TJQKA]+)([CDHS]+)", card, re.I)
+            match = re.match(r'([23456789TJQKA]+)([CDHS]+)', card, re.I)
             if not match:
                 raise Exception('wrong card defined')
             else:
                 values = match.groups()
                 self.numbers[values[0]] += 1
                 self.suits[values[1]] += 1
+
+    @staticmethod
+    def from_string(cards_str):
+        cards = cards_str.split(' ')
+        return Hand(cards)
+
+    @property
+    def value(self):
+        numbers = self.numbers.values()
+        qnt_suits = len(self.suits.values())
+        qnt_numbers = len(numbers)
+        msg = 'HIGH_CARD'
+
+        if qnt_numbers == 2 and qnt_suits > 1:
+            if 4 in numbers:
+                return 'FOUR_OF_A_KIND'
+            return 'FULL_HOUSE'
+        elif qnt_numbers == 3 and qnt_suits > 1:
+            if 3 in numbers:
+                return 'TREE_OF_A_KIND'
+            return 'TWO_PAIR'
+        elif qnt_numbers == 4 and qnt_suits > 1:
+            return 'ONE_PAIR'
+        else:
+            flush, straight = False, False
+            lower_card = min([self.possible_numbers.index(x) for x in self.numbers.keys()])
+            higher_card = max([self.possible_numbers.index(x) for x in self.numbers.keys()])
+            low_straight = set(("A", "2", "3", "4", "5"))
+
+            if qnt_suits == 1:
+                flush = True
+                msg = 'FLUSH'
+            if higher_card - lower_card == 4 or not set(self.numbers.keys()).difference(low_straight):
+                straight = True
+                msg = 'STRAIGHT'
+
+            if straight and flush:
+                if self.possible_numbers[higher_card] is 'A':
+                    return 'ROYAL_FLUSH'
+                return 'STRAIGHT_FLUSH'
+
+            return msg
--- src/test.py
+++ src/test.py
@@ -5,12 +5,12 @@ from hand import Hand
 class TestPokerHand(unittest.TestCase):
 
     def test_hand_must_be_created_by_method_from_string(self):
-        hand = Hand.from_string('4D 4D 4D 7H 8D')
+        hand = Hand.from_string('4D 4D 4S 7H 8D')
         self.assertTrue(isinstance(hand, Hand))
 
     def test_hand_wrong_card_parameter_should_raise_exception(self):
         with self.assertRaises(Exception) as cm:
-            Hand.from_string('4D 4D 4D 99k 8D')
+            Hand.from_string('4D 4D 4S 99k 8D')
         self.assertEquals(cm.exception.message, 'wrong card defined')
 
     def test_less_than_5_cards_should_raise_exception(self):
@@ -23,16 +23,59 @@ class TestPokerHand(unittest.TestCase):
         self.assertEquals(cm.exception.message, '8 cards in the hand, must be 5')
 
     def test_hand_must_count_numbers(self):
-        hand = Hand.from_string('4D 4D 4D 7H 8D')
+        hand = Hand.from_string('4D 4D 4S 7H 8D')
         self.assertEquals(hand.numbers['4'], 3)
         self.assertEquals(hand.numbers['7'], 1)
         self.assertEquals(hand.numbers['8'], 1)
 
     def test_hand_must_count_suits(self):
-        hand = Hand.from_string('4D 4D 4D 7H 8D')
-        self.assertEquals(hand.suits['D'], 4)
-        self.assertEquals(hand.suits['H'], 1)
+        hand = Hand.from_string('4D 4D 4H 7H 8D')
+        self.assertEquals(hand.suits['D'], 3)
+        self.assertEquals(hand.suits['H'], 2)
 
+    def test_hand_with_a_high_card_should_return_HIGH_CARD_as_value(self):
+        hand = Hand.from_string('4D 5D 3D 7H KD')
+        self.assertEquals(hand.value, 'HIGH_CARD')
+
+    def test_hand_with_one_pair_should_return_ONE_PAIR_as_value(self):
+        hand = Hand.from_string('4D 4D 2D 7H KD')
+        self.assertEquals(hand.value, 'ONE_PAIR')
+
+    def test_hand_with_two_pairs_should_return_TWO_PAIRS_as_value(self):
+        hand = Hand.from_string('4D 4D 7H 7H KD')
+        self.assertEquals(hand.value, 'TWO_PAIR')
+
+    def test_hand_with_same_tree_cards_should_return_TREE_OF_A_KIND_as_value(self):
+        hand = Hand.from_string('4D 4D 4S 7H KD')
+        self.assertEquals(hand.value, 'TREE_OF_A_KIND')
+
+    def test_hand_with_same_four_cards_should_return_FOUR_OF_A_KIND_as_value(self):
+        hand = Hand.from_string('4D 4D 4C 4C KS')
+        self.assertEquals(hand.value, 'FOUR_OF_A_KIND')
+
+    def test_hand_with_same_tree_cards_and_a_pair_should_return_FULL_HOUSE_as_value(self):
+        hand = Hand.from_string('4D 4D 4C 3D 3D')
+        self.assertEquals(hand.value, 'FULL_HOUSE')
+
+    def test_hand_with_same_all_suits_should_return_FLUSH_as_value(self):
+        hand = Hand.from_string('2D 4D TD 9D 2D')
+        self.assertEquals(hand.value, 'FLUSH')
+
+    def test_hand_with_sequence_numbers_should_return_STRAIGHT_as_value(self):
+        hand = Hand.from_string('2D 3D 4D 5D 6S')
+        self.assertEquals(hand.value, 'STRAIGHT')
+
+    def test_hand_with_sequence_numbers_and_the_same_suits_should_return_STRAIGHT_FLUSH_as_value(self):
+        hand = Hand.from_string('2D 3D 4D 5D 6D')
+        self.assertEquals(hand.value, 'STRAIGHT_FLUSH')
+
+    def test_hand_with_high_sequence_numbers_and_the_same_suits_should_return_ROYAL_FLUSH_as_value(self):
+        hand = Hand.from_string('TS JS QS KS AS')
+        self.assertEquals(hand.value, 'ROYAL_FLUSH')
+
+    def test_hand_with_sequence_cards_when_A_is_lower_card_should_return_STRAIGHT_as_value(self):
+        hand = Hand.from_string('AS 2S 3D 4S 5D')
+        self.assertEquals(hand.value, 'STRAIGHT')
 
 if __name__ == '__main__':
     unittest.main()
